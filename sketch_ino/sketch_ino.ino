2cf090c87a65960f2319c05cc7ec19981014cedc
GeorgeAlTomeh <georgetoumeh134@gmail.com>
2026-05-07 03:20:31 +0300

    adding wifi connection

--- sketch_ino/sketch_ino.ino
+++ sketch_ino/sketch_ino.ino
@@ -1,11 +1,20 @@
+#include <WiFi.h>
 #include <LiquidCrystal.h>
+
 LiquidCrystal lcd(27, 14, 26, 25, 33, 32);
 const int lm35Pin = 34;
 
+const char* ssid = "testWIFI";
+const char* password = "12345678";
+
 void setup() {
-  lcd.begin(16, 2);
-  lcd.print("Temp Monitor");
-  delay(2000);
+  lcd.begin(16,2);
+  WiFi.begin(ssid, password);
+  lcd.print("Connecting WiFi");
+  while (WiFi.status() != WL_CONNECTED) delay(500);
+  lcd.clear();
+  lcd.print("WiFi OK");
+  delay(1000);
   lcd.clear();
 }
 
